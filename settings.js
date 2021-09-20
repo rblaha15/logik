06a44356c42b6ec18b39902d0194d8b718073b5b
rblaha15 <rblaha15@student.gymceska.cz>
2021-09-20 07:29:31 +0200

    fes

--- settings.js
+++ settings.js
@@ -1,15 +1,33 @@
-let options = window.location.search.substr(1).split("&").map(function (it, i, a) { return it.split("=", 2); })
+const options = window.location.search.substr(1).split("&").map(function (it, i, a) { return it.split("=", 2) })
 
 console.log(options)
 
-let r = document.forms["settings"]["rows"].value;
-let easy = document.forms["settings"]["easyMode"].value;
-let rep = document.forms["settings"]["repeat"].value;
+for (pair of options) {
+    const a = pair[0]; const b = pair[1]
+    switch (a) {
+        case "repeating":
+            document.forms["settings"]["repeat"].value = b == "on"
+        case "easyMode":
+            document.forms["settings"]["easyMode"].value = b == "on"
+        case "rows":
+            if (b < 1) {
+                document.forms["settings"]["easyMode"].value = 1
+            } else {
+                document.forms["settings"]["easyMode"].value = b
+            }
+
+    }
+
+}
+
+let r = document.forms["settings"]["rows"].value
+let easy = document.forms["settings"]["easyMode"].value
+let rep = document.forms["settings"]["repeat"].value
 
 function checkForm() {
-    let r = document.forms["settings"]["rows"].value;
-    let easy = document.forms["settings"]["easyMode"].value;
-    let rep = document.forms["settings"]["repeat"].value;
+    let r = document.forms["settings"]["rows"].value
+    let easy = document.forms["settings"]["easyMode"].value
+    let rep = document.forms["settings"]["repeat"].value
     console.log(r)
     if (r == null) {
         document.forms["settings"]["rows"].value = 1
